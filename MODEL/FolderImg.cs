d21bd5e2256f377c2b81c309822279c8f1a104bf
gaolei-123 <79306320+gaolei-123@users.noreply.github.com>
2021-02-24 16:59:45 +0800

    新建三个数据类和文章管理的一些dal层功能

---
+++ MODEL/FolderImg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MODEL
+{
+    public partial class FolderImg
+    {
+        public int Id { get; set; }
+        public int? FolderId { get; set; }
+        public string Name { get; set; }
+        public string Countnt { get; set; }
+        public int? Sort { get; set; }
+        public int? Status { get; set; }
+        public string Img { get; set; }
+        public string JumpUrl { get; set; }
+        public DateTime? CreateTime { get; set; }
+
+        public virtual Folder Folder { get; set; }
+    }
+}
