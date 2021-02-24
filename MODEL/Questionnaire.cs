3b45a71b1c4c1b55a2ef3ca2ecb6da904e62c000
Lby23 <2365064920@qq.com>
2021-02-24 16:56:55 +0800

    问卷调查模型类

---
+++ MODEL/Questionnaire.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MODEL
+{
+    public class Questionnaire //问卷表
+    {
+        public int QuesId       { get; set; }//主键
+        public string Title        { get; set; }//问卷标题
+        public string Sno          { get; set; }//问卷编号
+        public bool Qsale        { get; set; }//问卷状态
+        public int Snum         { get; set; }//回收数量
+        public DateTime CreateTime   { get; set; }//创建时间
+        public string CreatePeople { get; set; }//创建人
+        public string BeginMs      { get; set; }//开始描述
+        public string EndMs        { get; set; }//结束描述
+        public int TopicId { get; set; }     //--问卷表外键关联问题表
+    }
+}
