faeae92fe56c830b84c38b52fcd2956e9ced9814
gaolei-123 <79306320+gaolei-123@users.noreply.github.com>
2021-02-25 19:03:51 +0800

    文章管理内容里的dal和控制器里新增的一些方法

--- DAL/ArticleDAL.cs
+++ DAL/ArticleDAL.cs
@@ -10,7 +10,7 @@ namespace DAL
         //文章管理查询显示
         public List<Article> GetArticles(string artname, string folname, int status)
         {
-            string strSql = "select * from article join folder on article.Folder_Id =folder.Id";
+            string strSql = "select article.*,folder.Name from article join folder on article.FolderId =folder.Id";
             strSql += " and 1=1";
             if (!string.IsNullOrEmpty(artname))
             {
@@ -18,7 +18,7 @@ namespace DAL
             }
             if (!string.IsNullOrEmpty(folname))
             {
-                strSql += $"where article.Name like '%{folname}%'";
+                strSql += $"where article.Title like '%{folname}%'";
             }
             if (status != 0)
             {
@@ -27,10 +27,17 @@ namespace DAL
             return NewDBHelper.GetList<Article>(strSql);
         }
 
+        //栏目绑定下拉框
+        public List<Folder> GetFolders()
+        {
+            string strSql = $"select * from folder";
+            return NewDBHelper.GetList<Folder>(strSql);
+        }
+
         //文章管理添加
         public int AddArticle(Article a)
         {
-            string strSql = $"insert into article values('{a.FolderId}','{a.Title}','{a.Sort}','{a.Status}','{a.IsUp}','{a.IsComment}','{a.IsRecommend}','{a.ApproveStatus}','{a.CreateTime}','{a.StartTime}','{a.EndTime}','{a.Type}','{a.JumpUrl}','{a.Image}','{a.CreatePeople}')";
+            string strSql = $"insert into article values('{a.FolderId}','{a.Title}','{a.Sort}','{a.Status}','{a.IsUp}','{a.IsComment}','{a.IsRecommend}','{a.ApproveStatus}',getdate(),'{a.StartTime}','{a.EndTime}','{a.Type}','{a.JumpUrl}','{a.Image}','{a.CreatePeople}')";
             return NewDBHelper.ExecuteNonQuery(strSql);
         }
 
@@ -41,6 +48,15 @@ namespace DAL
             return NewDBHelper.ExecuteNonQuery(strSql);
         }
 
+        //文章管理修改
+        public int UpdArticle(Article a)
+        {
+            string strSql = $"update article set FolderId='{a.FolderId}',Title='{a.Title}',Sort='{a.Sort}',Status='{a.Status}',IsUp='{a.IsUp}',IsComment='{a.IsComment}'," +
+                $"Is_recommend='{a.IsRecommend}',Approve_status='{a.ApproveStatus}',CreateTime='{a.CreateTime}',Start_Time='{a.StartTime}',End_Time='{a.EndTime}'," +
+                $"Type='{a.Type}',Jump_url='{a.JumpUrl}',Image='{a.Image}',CreatePeople='{a.CreatePeople}'";
+            return NewDBHelper.ExecuteNonQuery(strSql);
+        }
 
+        //文章管理
     }
 }
---
+++ 监察中心API/Controllers/ArticleController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MODEL;
+using DAL; 
+
+namespace 监察中心API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        //实例化dal层
+        ArticleDAL dal = new ArticleDAL();
+
+        [HttpGet]
+        [EnableCors("any")]
+        //调用dal层显示方法
+        public ObjectResult GetArticles(string artname="", string folname="", int status=0)
+        {
+            var data = dal.GetArticles(artname, folname, status);
+            return Ok(new {data=data,code=0});
+        }
+
+        [HttpGet]
+        [EnableCors("any")]
+        //调用dal层绑定下拉方法
+        public ObjectResult GetFolder()
+        {
+            var data = dal.GetFolders();
+            return Ok(new { data = data});
+        }
+
+        [HttpPost]
+        [EnableCors("any")]
+        //调用dal层添加方法
+        public int Create(Article a)
+        {
+            return dal.AddArticle(a);
+        }
+
+        [HttpDelete]
+        [EnableCors("any")]
+        //调用dal层删除方法
+        public int Delete(int id)
+        {
+            return dal.DelArticle(id);
+        }
+
+        [HttpPost]
+        [EnableCors("any")]
+        //调用dal层修改方法
+        public int Update(Article a)
+        {
+            return dal.UpdArticle(a);
+        }
+    }
+}
