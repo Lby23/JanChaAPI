0eb7e3abfb5c42c60f4fad151b5f51eb7d6ebd20
gaolei-123 <79306320+gaolei-123@users.noreply.github.com>
2021-03-01 14:04:28 +0800

    在原本方法上进行更改

--- DAL/ArticleDAL.cs
+++ DAL/ArticleDAL.cs
@@ -8,13 +8,13 @@ namespace DAL
     public class ArticleDAL
     {
         //文章管理查询显示
-        public List<Article> GetArticles(string artname, string folname, int status)
+        public List<Article> GetArticles(int artname, string folname, int status,int page,int limit)
         {
             string strSql = "select article.*,folder.Name from article join folder on article.FolderId =folder.Id";
             strSql += " and 1=1";
-            if (!string.IsNullOrEmpty(artname))
+            if (artname != 0)
             {
-                strSql += $"where folder.Name like '%{artname}%'";
+                strSql += $"where article.folderId = '{artname}'";
             }
             if (!string.IsNullOrEmpty(folname))
             {
@@ -37,7 +37,8 @@ namespace DAL
         //文章管理添加
         public int AddArticle(Article a)
         {
-            string strSql = $"insert into article values('{a.FolderId}','{a.Title}','{a.Sort}','{a.Status}','{a.IsUp}','{a.IsComment}','{a.IsRecommend}','1',getdate(),'{a.StartTime}','{a.EndTime}','{a.Type}','{a.JumpUrl}','{a.Image}','{a.CreatePeople}')";
+            string strSql = $"insert into article values('{a.FolderId}','{a.Title}','{a.Sort}','{a.Status}','1','{a.IsComment}','{a.IsRecommend}','1'," +
+                $"getdate(),'{a.StartTime}','{a.EndTime}','{a.Type}','{a.JumpUrl}','{a.Image}','{a.CreatePeople}')";
             return NewDBHelper.ExecuteNonQuery(strSql);
         }
 
@@ -51,9 +52,9 @@ namespace DAL
         //文章管理修改
         public int UpdArticle(Article a)
         {
-            string strSql = $"update article set FolderId='{a.FolderId}',Title='{a.Title}',Sort='{a.Sort}',Status='{a.Status}',IsUp='{a.IsUp}',IsComment='{a.IsComment}'," +
-                $"Is_recommend='{a.IsRecommend}',Approve_status='{a.ApproveStatus}',CreateTime='{a.CreateTime}',Start_Time='{a.StartTime}',End_Time='{a.EndTime}'," +
-                $"Type='{a.Type}',Jump_url='{a.JumpUrl}',Image='{a.Image}',CreatePeople='{a.CreatePeople}'";
+            string strSql = $"update article set FolderId='{a.FolderId}',Title='{a.Title}',Sort='{a.Sort}',Status='{a.Status}',IsUp='1',IsComment='{a.IsComment}'," +
+                $"IsRecommend='{a.IsRecommend}',ApproveStatus='1',CreateTime='{a.CreateTime}',StartTime='{a.StartTime}',EndTime='{a.EndTime}'," +
+                $"Type='{a.Type}',JumpUrl='{a.JumpUrl}',Image='{a.Image}',CreatePeople='{a.CreatePeople}' where Id='{a.Id}'";
             return NewDBHelper.ExecuteNonQuery(strSql);
         }
 
