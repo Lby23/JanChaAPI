45eac3116ed53cd80d2746f86ede54e49be49fe4
点豆豆 <@NICE>
2021-03-01 15:59:36 +0800

    1

--- MODEL/User.cs
+++ MODEL/User.cs
@@ -5,11 +5,13 @@ namespace MODEL
     public partial class User
     {
         public int UId		 { get; set; }
-        public string UserName { get; set; }
+        public string UserName { get; set; }//用户名
         public bool UserSex  { get; set; }
         public int Age		 { get; set; }
-        public string Phone	 { get; set; }
-        public string Number   { get; set; }
-        public string Password { get; set; }
+        public string Phone	 { get; set; }//手机号
+        public int Emile { get; set; }//电子邮件
+        public string Note { get; set; }//备注
+        public string Number   { get; set; }//账号
+        public string Password { get; set; }//密码
     }
 } 
