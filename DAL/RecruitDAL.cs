af6f699b98037b5367da4b3e6c87267b92264786
永涛 高 <2810871340@qq.com>
2021-03-02 12:00:23 +0800

    黑名单表 举报投诉表 招聘管理表 DAL层与控制器完成

---
+++ DAL/RecruitDAL.cs
@@ -0,0 +1,36 @@
+﻿using MODEL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    //招聘管理
+    public class RecruitDAL
+    {
+        public List<Recruit> GetRecruits(string RPosition)
+        {
+            string strSql = $"select * from Recruit where 1=1";
+            if (RPosition != null)
+            {
+                strSql += $" and RPosition='{RPosition}'";
+            }
+            return NewDBHelper.GetList<Recruit>(strSql);
+        }
+        public int AddRecruits(Recruit rc)
+        {
+            string strSql = $"insert into Recruit values('{rc.Rtype}','{rc.Rposition}','{rc.RpositionDescride}','{rc.Rlocation}','{rc.Rcompany}','{rc.Rstate}','{rc.Rtime}','{rc.Rperson}')";
+            return NewDBHelper.ExecuteNonQuery(strSql);
+        }
+        public int DeleteRecruits(int RId)
+        {
+            string strSql = $"delete from Recruit where RId={RId}";
+            return NewDBHelper.ExecuteNonQuery(strSql);
+        }
+        public int UpdateRecruits(Recruit rc)
+        {
+            string strSql = $"update Recruit set RType='{rc.Rtype}',RPosition='{rc.Rposition}',RPositionDescride='{rc.RpositionDescride}',RLocation='{rc.Rlocation}',RCompany='{rc.Rcompany}',RState='{rc.Rstate}',RTime='{rc.Rtime}',RPerson='{rc.Rperson}' where RId='{rc.Rid}'";
+            return NewDBHelper.ExecuteNonQuery(strSql);
+        }
+    }
+}
---
+++ 监察中心API/Controllers/RecruitController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MODEL;
+using DAL;
+using System.Text.Json;
+
+namespace 监察中心API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecruitController : ControllerBase
+    {
+        RecruitDAL rcdal = new RecruitDAL();
+        [HttpGet]
+        public string GetRecruits(string RPosition,int page,int limit)
+        {
+            List<Recruit> data = rcdal.GetRecruits(RPosition);
+            int count = data.Count();
+            data = data.Skip((page - 1) * limit).Take(limit).ToList();
+            var json = JsonSerializer.Serialize(new { data = data, code = 0, count = count });
+            return json;
+        }
+        [HttpPost]
+        public int AddRecruits(Recruit rc)
+        {
+            int i = rcdal.AddRecruits(rc);
+            return i;
+        }
+        [HttpDelete]
+        public int DeleteRecruits(int RId)
+        {
+            int i = rcdal.DeleteRecruits(RId);
+            return i;
+        }
+        [HttpPut]
+        public int UpdateRecruits(Recruit rc)
+        {
+            int i = rcdal.UpdateRecruits(rc);
+            return i;
+        }
+    }
+}
