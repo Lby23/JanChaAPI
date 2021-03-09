6930a94cb24c32928c4e92a3956953189fd44492
点豆豆 <@NICE>
2021-03-09 17:05:24 +0800

    1

---
+++ MODEL/tab.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MODEL
+{
+    public class tab
+    {
+        public int      TId { get; set; } 
+        public string Tittle { get; set; }
+        public string Tittle1 { get; set; }
+        public string Tittle11 { get; set; }
+        public string Tittle12 { get; set; }
+        public string Tittle2 { get; set; }
+        public string Tittle21 { get; set; }
+        public string Tittle22 { get; set; }
+        public string Tittle23 { get; set; }
+        public string Severce1 { get; set; }
+        public string Vocation { get; set; }
+    }
+}
